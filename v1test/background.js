92fbbb3df6fd1bc23ecdb927102d666d611cbcf2
vishnudk <vishnudk179@gmail.com>
2020-11-05 16:53:16 +0530

    added context menu for the extension

--- v1test/background.js
+++ v1test/background.js
@@ -1,11 +1,11 @@
 
-// var item = {
-//     "id":"hello world",
-//     "title":"the fiver extension",
-//     "contexts":['selection']
-// };
-// chrome.contextMenus.create(item);
-
-// // chrome.contexMenus.onClicked.addListener(function(clickData,tab){
-// //     console.log(clickData)
-// // });
+var item = {
+    "id":"hello world",
+    "title":"the fiver extension",
+    "contexts":['selection']
+};
+chrome.contextMenus.create(item);
+chrome.contextMenus.onClicked.addListener(function(clickData,tab){
+    console.log(clickData)
+});
+// console.log("hello world");
---
+++ v1test/public/jsFiles/background.js
@@ -0,0 +1,12 @@
+
+// var item = {
+//     "id":"hello world",
+//     "title":"the fiver extension",
+//     "contexts":['selection']
+// };
+// chrome.contextMenus.create(item);
+
+// // chrome.contexMenus.onClicked.addListener(function(clickData,tab){
+// //     console.log(clickData)
+// // });
+console.log("hello world");
