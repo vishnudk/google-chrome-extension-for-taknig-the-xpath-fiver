787ee80fc54caef3e18a58b5f4a881a058650f5d
vishnudk <vishnudk179@gmail.com>
2020-11-07 19:57:50 +0530

    added comments

--- v1test/background.js
+++ v1test/background.js
@@ -1,18 +1,17 @@
 
-var item = {
+var item = {                      // details for manking a context menu item 
     "id":"hello world",
     "title":"Get xpath",
     "contexts":['selection']
 };
-var data = '';
-console.log("helloo bro")
-chrome.contextMenus.create(item);
-chrome.contextMenus.onClicked.addListener(function(clickData,tab){
-    var sel = document.getElementById('scripts');
-    data = Object.keys(clickData);
+var data = ''; // just to make a global var
+chrome.contextMenus.create(item); // to create a context menu item
+chrome.contextMenus.onClicked.addListener(function(clickData,tab){  //function to be called when the extension is invoked
+    // var sel = document.getElementById('scripts');
+    data = Object.keys(clickData);            
     // alert(data)
-    chrome.tabs.executeScript({
-        code: "console.log(window.location.href)"
+    chrome.tabs.executeScript({                    // function to print in google chrome console
+        code: "console.log(window.location.href)" //used a js function to get the current url
       })
     
 });
