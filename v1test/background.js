6d89d4e3746a198977b9b648126e10c2417f4f85
vishnudk <vishnudk179@gmail.com>
2020-11-07 13:43:50 +0530

    changed it in to a xml path finding extension

--- v1test/background.js
+++ v1test/background.js
@@ -1,61 +1,69 @@
 
-// var item = {
-//     "id":"hello world",
-//     "title":"the fiver extension",
-//     "contexts":['selection']
-// };
-// console.log("helloo bro")
-// chrome.contextMenus.create(item,function(){
-//     console.log("hello bro...")
-// });
-// chrome.contextMenus.onClicked.addListener(function(clickData,tab){
-//     console.log(clickData)
-// });
+var item = {
+    "id":"hello world",
+    "title":"the fiver extension",
+    "contexts":['selection']
+};
+console.log("helloo bro")
+chrome.contextMenus.create(item,function(){
+    console.log("hello bro...")
+});
+chrome.contextMenus.onClicked.addListener(function(clickData,tab){
+    console.log(clickData);
+});
 // console.log("hello world");
-blockedUrls = [
-    "*://*.doubleclick.net/*",
-	"*://partner.googleadservices.com/*",
-	"*://*.googlesyndication.com/*",
-	"*://*.google-analytics.com/*",
-	"*://creative.ak.fbcdn.net/*",
-	"*://*.adbrite.com/*",
-	"*://*.exponential.com/*",
-	"*://*.quantserve.com/*",
-	"*://*.scorecardresearch.com/*",
-    "*://*.zedo.com/*",
-    "*://*.indiafightscovid.com/*",
-    "*://*.tabnine.com/*",
-    "*://*.brands.zee5.com/*",
-    "*://*.zapmeta.co.in/*",
-    "*://*.ads.google.com/*",
-    "*://*.izito.co.in/*",
-]
-// chrome.tabs.executeScript({
-//     code: 'console.log("addd")'
-// })
-// const log = chrome.extension.getBackgroundPage().console.log;
-// log('something')
-// chrome.extension.getBackgroundPage().console.log('foo');
+// blockedUrls = [
+//     "*://*.doubleclick.net/*",
+// 	"*://partner.googleadservices.com/*",
+// 	"*://*.googlesyndication.com/*",
+// 	"*://*.google-analytics.com/*",
+// 	"*://creative.ak.fbcdn.net/*",
+// 	"*://*.adbrite.com/*",
+// 	"*://*.exponential.com/*",
+// 	"*://*.quantserve.com/*",
+// 	"*://*.scorecardresearch.com/*",
+//     "*://*.zedo.com/*",
+//     "*://*.indiafightscovid.com/*",
+//     "*://*.tabnine.com/*",
+//     "*://*.brands.zee5.com/*",
+//     "*://*.zapmeta.co.in/*",
+ 
+//     "*://*.izito.co.in/*",
+// ]
+// // chrome.tabs.executeScript({
+// //     code: 'console.log("addd")'
+// // })
+// // const log = chrome.extension.getBackgroundPage().console.log;
+// // log('something')
+// // chrome.extension.getBackgroundPage().console.log('foo');
 
 
 
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details){
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function(details){
         
-    //     if (window.location.href != "*://*.youtube.com/*"){
-    //         return {cancel:true}
-    // }
-    return {cancel:true}
-},
-    {
-        urls: blockedUrls
+//     //     if (window.location.href != "*://*.youtube.com/*"){
+//     //         return {cancel:true}
+//     // }
+//     return {cancel:true}
+// },
+//     {
+//         urls: blockedUrls
         
-    },
-    ['blocking']
+//     },
+//     ['blocking']
     
-)
-console.log("hello world ")
-console.log(chrome.webRequest.urls)
+// )
+// function myFunction() { 
+//     //prints in console 
+//     console.log("log:Tony sent a message");  
+    
+//     //prints in console 
+//     console.info("info:Quill recieved.");  
+// } 
+// myFunction()
+// console.log("hello world ")
+// console.log(chrome.webRequest.urls)
 
 
 // console.log("Loaded extension");
